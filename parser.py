c4ea79fd7facaccf676bb6dbe6071f1eb1ef84d7
fafa_junhe <842355358@qq.com>
2020-04-14 11:24:47 +0800

    fix bugs

--- parser.py
+++ parser.py
@@ -4,69 +4,51 @@
 import base64
 import multiprocessing
 import socket  # 导入 socket 模块
+import time
 from multiprocessing import Queue
 from time import sleep
 import threading
 
-Head = '[TextWar]'
-Time_stamp = 'Time-Stamp'
-Data_length = 'Data-Length'
 
+def get_format(json):
+    return """[TextWar]\r\nTime-Stamp: {}\r\nData-Length: {}\r\n\r\n{}""".format(int(time.time()), len(base64.b64encode(
+                                                                                     json.encode())),
+                                                                                 base64.b64encode(
+                                                                                     json.encode())).encode()
 
 class TextWarProtocol(Exception):
     """Base class for exceptions in this module."""
     pass
 
 
-class Client(object):
-    def __init__(self, server, port):
-        self.p = None
-        self.server = server
-        self.port = port
-        self.before_pak_time = 0
+class Client:
+    def __init__(self):
+        self.s = socket.socket()  # 创建套接字
 
-    def loop(self, output):
-        s = socket.socket()  # 创建 socket 对象
-        while True:
-            try:
-                s.connect((self.server, self.port))
-            except Exception as e:
-                sleep(0.5)
-                print("Cannot connect ", e)
-                continue
-            break
-        while True:
-            out = s.recv(1024).decode().replace("\r", "")
-            list_ = out.split("\n")
-            if len(list_) != 5:
-                raise TextWarProtocol("line count of message expected " + "5" + ", got " + str(len(list_)) + ".")
-            if list_[0] != Head:
-                raise TextWarProtocol("head expected " + Head + ", got " + list_[0] + ".")
-            if not list_[1].startswith(Time_stamp):
-                raise TextWarProtocol("line 2 expected " + Time_stamp + ": TIME_STAMP_HERE" + ", got " + list_[1] + ".")
-            if not list_[2].startswith(Data_length):
-                raise TextWarProtocol("line 3 expected " + Data_length + ": DATA_LENGTH_HERE" + ", got " + list_[2] + ".")
-            if not list_[3] == '':
-                raise TextWarProtocol("line 4 expected " + "BLANK" + ", got " + list_[3] + ".")
-            if int(list_[1].split(" ")[1]) < self.before_pak_time:
-                raise TextWarProtocol("Time slower than the before one ", list_[1])
-            self.before_pak_time = int(list_[1].split(" ")[1])
-            output.put(base64.b64decode(list_[4]).decode())
+    def connect(self, server, port):
+        while self.s.connect_ex((server, port)) == 111:
+            pass
+        return self
 
-    def start(self, out):
-        self.p = multiprocessing.Process(target=self.loop, args=(out,))
-        self.p.start()
 
-    def out(self):
-        return self.p.output
+    def send(self, json):
+        print("aa")
+        self.s.sendall(get_format(json))
+        return self.s.recv(1024).decode()
 
     def stop(self):
-        self.p.s.stop()
-        self.p.terminate()
+        self.s.close()
+        # self.s.sendall("exit".encode())
 
+client = Client()
+client.connect(socket.gethostname(), 31244)
+client.send("{'hello':'world'}")
+client.stop()
 
-if __name__ == "__main__":
-    a = Queue(maxsize=1)
-    Client(socket.gethostname(), 31244).start(a)
-    while 1:
-        print(a.get())
+       # 关闭连接
+
+# if __name__ == "__main__":
+#     a = Queue(maxsize=1)
+#     Client(socket.gethostname(), 31244).start(a)
+#     while 1:
+#         print(a.get())
--- sender.py
+++ sender.py
@@ -5,44 +5,66 @@ import base64
 import multiprocessing
 import socket  # 导入 socket 模块
 import sys
+import threading
 import time
+Head = '[TextWar]'
+Time_stamp = 'Time-Stamp'
+Data_length = 'Data-Length'
 
 
-def get_format(json):
-    return """[TextWar]\r\nTime-Stamp: {}\r\nData-Length: {}\r\n\r\n{}""".format(int(time.time()), len(json),
-                                                                                 base64.b64encode(
-                                                                                     json.encode()).decode())
+class TextWarProtocol(Exception):
+    """Base class for exceptions in this module."""
+    pass
 
-
-class Server(object):
-    def __init__(self, server, port):
+class Server(threading.Thread):
+    def __init__(self, server, port, result):
+        super().__init__()
         self.port = port
         self.server = server
-        self.p = None
+        self.s = socket.socket()
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    def start(self, json):
-        self.p = multiprocessing.Process(target=self.loop, args=(json,))
-        self.p.start()
+        self.s.bind((server, port))  # 绑定服务地址
+        self.s.listen(5)
+        self.link = None
+        self.client = None
+        self.before_pak_time = 0
+        self.result = result
 
-    def loop(self, json):
-        while True:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-            s.bind((self.server, self.port))  # 绑定端口
-
-            s.listen(0)  # 等待客户端连接
-            c, addr = s.accept()  # 建立客户端连接
-            while 1:
-                time.sleep(0.5)
-                c.send(get_format(json.get()).encode())
+    def connect(self):
+        self.link, self.client = self.s.accept()
 
 
-    def stop(self):
-        self.p.terminate()
-        self.p.c.close()
-        self.p.s.close()
+    def run(self):
+        print("服务器开始接收来自[%s:%s]的请求...." % (self.client[0], self.client[1]))
+        while True:  # 利用一个死循环，保持和客户端的通信状态
+            out = self.link.recv(1024).decode().replace("\r","")
+            if out == "":
+                continue
+            if out == "exit":
+                print("接收到exit")
+                break
+            print("来自[%s:%s]的客户端向你发来信息：\n%s" % (self.client[0], self.client[1], out))
+            list__ = out.split("\n")
+            if len(list__) != 5:
+                raise TextWarProtocol("line count of message expected " + "5" + ", got " + str(len(list__)) + ".")
+            if list__[0] != Head:
+                raise TextWarProtocol("head expected " + Head + ", got " + list__[0] + ".")
+            if not list__[1].startswith(Time_stamp):
+                raise TextWarProtocol("line 2 expected " + Time_stamp + ": TIME_STAMP_HERE" + ", got " + list__[1] + ".")
+            if not list__[2].startswith(Data_length):
+                raise TextWarProtocol("line 3 expected " + Data_length + ": DATA_LENGTH_HERE" + ", got " + list__[2] + ".")
+            if not list__[3] == '':
+                raise TextWarProtocol("line 4 expected " + "BLANK" + ", got " + list__[3] + ".")
+            if int(list__[1].split(" ")[1]) < self.before_pak_time:
+                raise TextWarProtocol("Time slower than the before one ", list__[1])
+            if int(list__[2].split(": ")[1]) != len(list__[4][2:][:-1]):
+                raise TextWarProtocol(f"Data-Length not match the json data, expected {len(list__[4])}, got {len(list__[4][2:][:-1])}")
+            self.before_pak_time = int(list__[1].split(" ")[1])
+            self.link.sendall('服务器已经收到你的信息'.encode())
+            self.result = base64.b64decode(list__[4])
+
+
 event = """
 {
     "type": "event",
@@ -59,12 +81,10 @@ if __name__ == "__main__":
     try:
         host = socket.gethostname()  # 获取本地主机名
         port = 31244  # 设置端口
-        a = Server(host, port)
-        ab = multiprocessing.Queue(1)
-        a.start(ab)
-
-        while True:
-            ab.put(event)
+        list_ = []
+        a = Server(host, port, list_)
+        a.connect()
+        a.start()
         # a.stop()
         # sys.exit()
     except (SystemExit, KeyboardInterrupt):
